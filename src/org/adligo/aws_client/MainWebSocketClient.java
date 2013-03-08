276b3b9c1d10e56437bce8cea5defad324486d1f
scott <scott@adligo.com>
2013-03-08 08:29:27 +0000

    yea reading and writing to jetty 8.1.8 is working, still much to do but nice to see it go

--- src/org/adligo/aws_client/MainWebSocketClient.java
+++ src/org/adligo/aws_client/MainWebSocketClient.java
@@ -6,24 +6,30 @@ import java.io.InputStream;
 import java.net.URI;
 import java.util.HashMap;
 
+import org.adligo.aws_client.models.WebSocketConnectionStates;
+import org.adligo.aws_client.models.WebSocketProtocol;
 import org.adligo.i.util.client.I_Event;
 import org.adligo.i.util.client.I_Listener;
 
 public class MainWebSocketClient {
-
+	private static boolean shutdown = false;
+	
 	public static void main(String [] args) {
 		try {
 			HashMap<String, String> headers = new HashMap<String, String>();
 			headers.put("key1", "value1");
 			headers.put("key2", "value2");
 
-			WebSocketClientConfig config = new WebSocketClientConfig();
+			WebSocketClientConfig config = new WebSocketClientConfig(WebSocketProtocol.RFC6544);
 			//config.setUrl(new URI("ws://localhost:8088/debug/"));
-			//config.setUrl(new URI("ws://localhost:8088/ws_server_demo/debug/a"));
-			config.setUrl(new URI("ws://localhost:8088/ws_server_demo/count/a"));
+			config.setUrl(new URI("ws://localhost:8088/ws_server_demo/debug/a"));
+			//config.setUrl(new URI("ws://localhost:8088/ws_server_demo/count/a"));
 			config.setHeaders(headers);
 			
 			final I_WebSocketClient ws = new WebSocketClient(config);
+			//add listener
+			I_WebSocketReader reader = config.getReader();
+			final Thread t = new Thread(reader);
 			ws.addListener(new I_Listener() {
 				@Override
 				public void onEvent(I_Event p) {
@@ -32,20 +38,30 @@ public class MainWebSocketClient {
 					if (p.threwException()) {
 						p.getException().printStackTrace();
 						ws.removeListener(this);
-						ws.disconnect();
+						shutdown(ws, t);
 					} else {
-						Integer num = Integer.parseInt((String) val);
-						if (num > 20) {
-							ws.disconnect();
+						if (val instanceof WebSocketConnectionStates) {
+							if (val == WebSocketConnectionStates.CONNECTION_CLOSED) {
+								shutdown(ws, t);
+							}
+						}
+						if (val instanceof String) { 
+							String content = (String) val;
+							if (content.indexOf("20") != -1) {
+									shutdown(ws, t);
+							}
 						}
 					} 
 				}
 			});
 			ws.connect();
 
+			
+			t.start();
+			
 			System.out.println("calling server ");
 			sendFile(ws);
-			/*
+			
 			ws.send("request 1");
 			Thread.sleep(500);
 			ws.send("request 2");
@@ -60,9 +76,15 @@ public class MainWebSocketClient {
 			System.out.println("disconnecting from server ");
 			ws.disconnect();
 			System.out.println("disconnected from server ");
+			/*
+			while (!shutdown) {
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException x) {
+					x.printStackTrace();
+				}
+			}
 			*/
-			
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -81,5 +103,18 @@ public class MainWebSocketClient {
 		}
 		String file = baos.toString("UTF-8");
 		ws.send(file);
+	}
+
+	private static void shutdown(final I_WebSocketClient ws, final Thread t) {
+		Exception e = new Exception("shutdown called from ");
+		e.printStackTrace();
+		
+		ws.disconnect();
+		try {
+			t.join();
+		} catch (InterruptedException x) {
+			x.printStackTrace();
+		}
+		shutdown = true;
 	}
 }
--- src/org/adligo/aws_client/WebSocketClientConfigTests.java
+++ src/org/adligo/aws_client/WebSocketClientConfigTests.java
@@ -1,12 +1,13 @@
 package org.adligo.aws_client;
 
 import org.adligo.aws_client.WebSocketClientConfig.OUTPUT_FORMAT;
+import org.adligo.aws_client.models.WebSocketProtocol;
 import org.adligo.tests.ATest;
 
 public class WebSocketClientConfigTests extends ATest {
 
 	public void testConfig() {
-		WebSocketClientConfig config = new WebSocketClientConfig();
+		WebSocketClientConfig config = new WebSocketClientConfig(WebSocketProtocol.RFC6544);
 		config.setOutputFormat(OUTPUT_FORMAT.UTF8_STRING);
 		assertEquals(OUTPUT_FORMAT.UTF8_STRING, config.getOutputFormat());
 	}
